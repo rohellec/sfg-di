993ac340d1c957716627f237358386c10023200a
Dima Rogov <ds.rogv@gmail.com>
2022-12-07 23:36:51 +0300

    Add bean lifecycle methods and post processors

---
+++ src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessor.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleBean) {
+            ((LifeCycleBean) bean).beforeInit();
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleBean) {
+            ((LifeCycleBean) bean).afterInit();
+        }
+        return bean;
+    }
+}
