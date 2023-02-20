3f7f287f507adaa448001f8939ea99ff725ee450
Dima Rogov <ds.rogv@gmail.com>
2023-02-20 21:52:16 +0300

    Getting PetService beans through factory in Java Configuration

--- src/main/java/com/springframework/pets/services/DogPetService.java
+++ src/main/java/com/springframework/pets/services/DogPetService.java
@@ -1,10 +1,5 @@
 package com.springframework.pets.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Profile({"dog", "default"})
 public class DogPetService implements PetService {
 
     public String getPetType(){
--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,15 +1,32 @@
 package guru.springframework.sfgdi.config;
 
+import com.springframework.pets.services.PetService;
+import com.springframework.pets.services.PetServiceFactory;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory() {
+        return new PetServiceFactory();
+    }
+
+    @Bean
+    @Profile("dog")
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Bean
+    @Profile({"cat", "default"})
+    PetService catPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("cat");
+    }
 
     @Bean("i18nService")
     @Profile({"ES", "default"})
